2526d928f94191424f93383651ba75304924c8af
kavyaankem <ankemkavyaa03@gmail.com>
2026-07-09 23:34:58 +0530

    Comparision operator

--- ComparisonOperators/ComparisonOperators.js
+++ ComparisonOperators/ComparisonOperators.js
@@ -2,7 +2,7 @@ function onClickToCheck() {
     debugger;
     let first = document.getElementById("txtFirst").value.toLowerCase();
     let second = document.getElementById("txtSecond").value.toLowerCase();
-    if(first == second){
+    if(first === second){
         document.getElementById("pResult").innerHTML = "True";
     }else{
         document.getElementById("pResult").innerHTML = "False";
